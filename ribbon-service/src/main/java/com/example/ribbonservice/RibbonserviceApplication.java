ac2410e7f8315b8fd4b5738f0f0adf8fe92eab4a
guo <11>
2018-04-13 15:07:13 +0800

    ss

--- ribbon-service/src/main/java/com/example/ribbonservice/RibbonserviceApplication.java
+++ ribbon-service/src/main/java/com/example/ribbonservice/RibbonserviceApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
+@ComponentScan
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class RibbonserviceApplication {
 
 	public static void main(String[] args) {

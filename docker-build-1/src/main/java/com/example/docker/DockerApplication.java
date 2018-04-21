56eedde24248b793b84599b57f0c439b1abff38e
guo <11>
2018-04-21 12:55:28 +0800

    ss

---
+++ docker-build-1/src/main/java/com/example/docker/DockerApplication.java
@@ -0,0 +1,23 @@
+package com.example.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/")
+	String home(){
+		return "Hello Docker World";
+	}
+
+}
